3389333746dfe510203a1a034c5312ec5d9fa62e
Falguni Tongse <falgunijivika@gmail.com>
2026-09-13 06:58:06 +0530

    Fix hosted avatar registration

--- LifeQuest/lifequest/backend/server.js
+++ LifeQuest/lifequest/backend/server.js
@@ -16,9 +16,21 @@ const achievementRoutes = require('./routes/achievement.routes');
 const app = express();
 
 app.use(helmet());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'https://falgunitongse.github.io',
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
